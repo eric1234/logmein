f7ae4f7ea6d5702a0d56872746f994d0041d3349
Eric Anderson <eric@pixelwareinc.com>
2010-06-27 20:11:46 -0400

    Allow rake tasks to be easily included in main app.

---
+++ lib/logmein.rb
@@ -0,0 +1,2 @@
+module LogMeIn
+end
---
+++ lib/logmein/rails_tasks.rb
@@ -0,0 +1,8 @@
+# Some glue to make it easy to include Rails-specific rake tasks in
+# your Rails application. Simply put the following at the bottom of
+# your Rakefile:
+#
+#   require 'logmein'
+#   require 'logmein/rails_tasks'
+glob = "#{Gem.searcher.find('logmein').full_gem_path}/lib/tasks/*.rake"
+Dir[glob].each {|ext| load ext}
