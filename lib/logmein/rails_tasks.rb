c65224538b7586e9433d17d70941adf0eea73ce9
Eric Anderson <eric@pixelwareinc.com>
2010-08-03 09:41:39 -0400

    Some cleanup
    Make the login user type="email". If browser supports it we might get
    validation for free and devices like the iPhone give us a better keyboard
    (the @ symbol and .com is displayed by default).
    
    Also we had a misc 'require' that was not necessary.

--- lib/logmein/rails_tasks.rb
+++ lib/logmein/rails_tasks.rb
@@ -2,7 +2,6 @@
 # your Rails application. Simply put the following at the bottom of
 # your Rakefile:
 #
-#   require 'logmein'
 #   require 'logmein/rails_tasks'
 glob = "#{Gem.searcher.find('logmein').full_gem_path}/lib/tasks/*.rake"
 Dir[glob].each {|ext| load ext}
