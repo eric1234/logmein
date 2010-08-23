6fbf019c8454ce69124d6a99822ea853e9c75ef9
Eric Anderson <eric@pixelwareinc.com>
2010-08-23 18:14:57 -0400

    Add some config (not happy but seems the best option for this one case)

--- lib/logmein.rb
+++ lib/logmein.rb
@@ -1,2 +1,14 @@
 module Logmein
+
+  # We try to keep configuration to a minimum as we want this to be
+  # a drop-in login that works well for like-minded apps. But the
+  # one config that there is no easy solution to is where to go
+  # after login. To solve this we check a few locations (in this order)
+  #
+  #   params[:return_to]  - This way a link can determine the location after login
+  #   session[:return_to] - A bit like the params version but uses session so is more hidden
+  #                         Also automatically used on authentication failure
+  #   Logmein.return_to   - A nice way to provide a default if the above are not set.
+  #   root_url            - Path of last resort
+  mattr_accessor :return_to
 end
--- logmein.gemspec
+++ logmein.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'logmein'
-  s.version = '0.0.4'
+  s.version = '0.0.5'
   s.homepage = 'http://wiki.github.com/eric1234/logmein/'
   s.author = 'Eric Anderson'
   s.email = 'eric@pixelwareinc.com'
