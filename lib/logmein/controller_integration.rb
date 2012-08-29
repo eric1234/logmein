1adaee8da1dc8dfa4d5dadd3198db940319861ab
Eric Anderson <eric@pixelwareinc.com>
2012-08-29 10:25:41 -0400

    Fix so it works in isolation

--- lib/logmein/controller_integration.rb
+++ lib/logmein/controller_integration.rb
@@ -26,6 +26,10 @@ module Logmein::ControllerIntegration
   # All actions require an authenticated record UNLESS it is in the
   # PUBLIC_ACTIONS constant.
   def authenticate
+    # If path not define then we must be in an isolated engine. In that
+    # case security doesn't apply (it does say it wants to be isolated).
+    return unless respond_to? :login_url
+
     is_public = self.class.const_defined?('PUBLIC_ACTIONS') &&
       self.class::PUBLIC_ACTIONS.collect(&:to_sym).include?(action_name.to_sym)
     unless is_public || authenticated_record
